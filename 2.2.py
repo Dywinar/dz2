31ffa049bfbe32a04bf3f48a1ee7d4d55ca425c3
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-09 19:54:03 +0300

    Add files via upload

---
+++ 2.2.py
@@ -0,0 +1,8 @@
+chislo1 = int(input("Введите число: "))
+chislo2 = int(input("Введите число: "))
+if chislo2 > chislo1:
+    print(chislo2)
+elif chislo1 > chislo2:
+    print(chislo1)
+else:
+    print("Числа равные")
---
+++ 2.3.py
@@ -0,0 +1,6 @@
+chislo1 = int(input("Введите число: "))
+chislo2 = int(input("Введите число: "))
+if (chislo2+ chislo1) % 2 == 0:
+    print("YES")
+else:
+    print("NO")
