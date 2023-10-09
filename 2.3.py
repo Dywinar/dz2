0963816db8e3f4439819434488d46bccec825d11
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-09 20:44:16 +0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,6 +1,6 @@
 chislo1 = int(input("Введите число: "))
 chislo2 = int(input("Введите число: "))
-if  and 1<= chislo1 <= 8 and  1<= chislo2 <= 8:
+if  1<= chislo1 <= 8 and  1<= chislo2 <= 8:
     if (chislo2+ chislo1) % 2 == 0 and (chislo1 != 5 and chislo2 != 5):
         print("YES")
     elif chislo1 == 5 and chislo2 == 5 :
