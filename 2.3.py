9befe81fb84a558aa22f1bbf7684f2bcc16d2ffc
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-09 20:35:57 +0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,8 +1,6 @@
 chislo1 = int(input("Введите число: "))
 chislo2 = int(input("Введите число: "))
-if (chislo2+ chislo1) % 2 == 0 and (chislo1 != 5 and chislo2 != 5) :
+if (chislo2+ chislo1) % 2 == 0 :
     print("YES")
-elif chislo1 == 5 and chislo2 == 5 :
-    print("NO")
 else:
     print("NO")
