688fa977592e080e9f9aaffff2dac3d5e1794a00
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-09 20:52:19 +0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,11 +1,6 @@
 chislo1 = int(input("Введите число: "))
 chislo2 = int(input("Введите число: "))
-if  1<= chislo1 <= 8 and  1<= chislo2 <= 8:
-    if (chislo2+ chislo1) % 2 == 0 and (chislo1 != 5 and chislo2 != 5):
-        print("YES")
-    elif chislo1 == 5 and chislo2 == 5 :
-        print("NO")
-    else:
-        print("NO")
+if (chislo1 + chislo2) % 2 == 0:
+    print("NO")
 else:
-    print("Число должно быть в диапозоне от 1 до 8")
+    print("YES")
