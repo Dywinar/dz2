f550925838e7290e975b3d35919de189dab3c482
Dywinar <144463363+Dywinar@users.noreply.github.com>
2023-10-09 21:01:50 +0300

    2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,6 +1,9 @@
 chislo1 = int(input("Введите число: "))
 chislo2 = int(input("Введите число: "))
-if (chislo1 + chislo2) % 2 == 0:
-    print("NO")
+if 1<= chislo1 <= 8 and 1<= chislo2 <= 8
+    if (chislo1 + chislo2) % 2 == 0:
+        print("NO")
+    else:
+        print("YES")
 else:
-    print("YES")
+print("Ввели не коретное число")
